abd6c270dbd8cd23adb0342a362485d9832bc9ee
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-12-04 23:49:45 +0200

    Viikko5

---
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -0,0 +1,137 @@
+from enum import Enum
+from tkinter import ttk, constants, StringVar
+
+
+class Komento(Enum):
+    SUMMA = 1
+    EROTUS = 2
+    NOLLAUS = 3
+    KUMOA = 4
+
+
+class Kayttoliittyma:
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._root = root
+        
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
+        }
+
+
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+    
+    def kaynnista(self):
+        self._arvo_var = StringVar()
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._syote_kentta = ttk.Entry(master=self._root)
+
+        tulos_teksti = ttk.Label(textvariable=self._arvo_var)
+
+        summa_painike = ttk.Button(
+            master=self._root,
+            text="Summa",
+            command=lambda: self._suorita_komento(Komento.SUMMA)
+        )
+
+        erotus_painike = ttk.Button(
+            master=self._root,
+            text="Erotus",
+            command=lambda: self._suorita_komento(Komento.EROTUS)
+        )
+
+        self._nollaus_painike = ttk.Button(
+            master=self._root,
+            text="Nollaus",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.NOLLAUS)
+        )
+
+        self._kumoa_painike = ttk.Button(
+            master=self._root,
+            text="Kumoa",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.KUMOA)
+        )
+
+        tulos_teksti.grid(columnspan=4)
+        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        summa_painike.grid(row=2, column=0)
+        erotus_painike.grid(row=2, column=1)
+        self._nollaus_painike.grid(row=2, column=2)
+        self._kumoa_painike.grid(row=2, column=3)
+
+    def _suorita_komento(self, komento):
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovelluslogiikka.arvo() == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._komennot[Komento.KUMOA].edellinen(komento_olio)
+
+class BinaariOperaatio:
+    def __init__(self, logiikka, io):
+        self.io = io
+        self.logiikka = logiikka
+        self.testi = 1
+
+    def suorita(self):
+        self.arvo = 0
+        try:
+            self.arvo = int(self.io())
+        except Exception:
+            pass
+        self.laske()
+
+class Summa(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+        
+    def laske(self):
+        self.edellinen = self.logiikka.arvo()
+        self.logiikka.plus(self.arvo)
+
+    def kumoa(self):
+        self.logiikka.aseta_arvo(self.edellinen)
+        
+class Erotus(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+        
+    def laske(self):
+        self.edellinen = self.logiikka.arvo()
+        self.logiikka.miinus(self.arvo)
+        
+    def kumoa(self):
+        self.logiikka.aseta_arvo(self.edellinen)
+        
+class Nollaus(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+        
+    def laske(self):
+        self.edellinen = self.logiikka.arvo()
+        self.logiikka.nollaa()
+        
+    def kumoa(self):
+        self.logiikka.aseta_arvo(self.edellinen)
+        
+class Kumoa(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+        
+    def suorita(self):
+        self.edellinen_olio.kumoa()
+        
+    def edellinen(self, edellinen_olio):
+        self.edellinen_olio = edellinen_olio
